93547aecef90a841fdcf699b1e6fbfbd27653461
Ekaterina-CyberDev <ekaterinakolbasina756@gmail.com>
2025-01-05 20:37:13 +0300

    Update main.go

--- mytest/main.go
+++ mytest/main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"bufio"
+	"database/sql"
+	"fmt"
 	"log"
-	"net/http"
-	"your_module_name/api"
-	"your_module_name/db"
+	"os"
+	"strings"
+
+	_ "github.com/lib/pq" // Импортируем драйвер PostgreSQL
 )
 
 func main() {
-	conn := db.ConnectDB()
-	defer conn.Close()
+	// Укажите свои параметры подключения к базе данных
+	connStr := "user=postgres password=1 dbname=tests sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	router := api.SetupRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// Проверка подключения
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Успешно подключено к базе данных!")
+
+	// Ввод данных от пользователя
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Введите вопрос: ")
+	question, _ := reader.ReadString('\n')
+	question = strings.TrimSpace(question) // Удаляем лишние пробелы и символы новой строки
+
+	fmt.Print("Введите варианты ответа: ")
+	variant_otveta, _ := reader.ReadString('\n')
+	variant_otveta = strings.TrimSpace(variant_otveta)
+
+	fmt.Print("Введите правильный ответ: ")
+	praviln_otvet, _ := reader.ReadString('\n')
+	praviln_otvet = strings.TrimSpace(praviln_otvet)
+
+	// Подготовка SQL запроса на вставку данных
+	insertQuery := `INSERT INTO test1 (question, variant_otveta, praviln_otvet) VALUES ($1, $2, $3)`
+
+	// Выполнение запроса для вставки данных
+	_, err = db.Exec(insertQuery, question, variant_otveta, praviln_otvet)
+	if err != nil {
+		log.Fatalf("Ошибка вставки данных: %v", err) // Выводим ошибку, если она произошла
+	}
+
+	// Успешное сообщение
+	fmt.Println("Данные успешно вставлены!")
 }
