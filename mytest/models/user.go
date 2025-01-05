c21be3749f0cffe09060065c9847f16725567a77
Ekaterina-CyberDev <ekaterinakolbasina756@gmail.com>
2025-01-05 20:37:28 +0300

    Delete mytest/models directory

--- mytest/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       int64  `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
-	Role     string `json:"role"` // Пользовательские роли (admin/user)
-}
